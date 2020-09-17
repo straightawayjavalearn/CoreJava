1275085807ed2a663f3b726f4385f6b1a526b5a7
nmuley <nmuley@nmuley-t460.apac.tibco.com>
2020-09-17 20:58:58 +0530

    done

--- OOPS/src/com/test/multithreading/MultiThreadingInterface.java
+++ OOPS/src/com/test/multithreading/MultiThreadingInterface.java
@@ -1,27 +1,18 @@
 package com.test.multithreading;
 
-class Student{
-	
-}
 
-interface address{
-	public void addAddress();
-}
-
-class MultiThreadingInterface extends Student implements Runnable,address{
+class MultiThreadingInterface  implements Runnable{
 	public void run() {
 		System.out.println("thread is running...");
 	}
 
 	public static void main(String args[]) {
-		MultiThreadingInterface m1 = new MultiThreadingInterface();
-		Thread t1 = new Thread(m1);
-		t1.start();
+		//MultiThreadingInterface m1 = new MultiThreadingInterface();
+		//Thread t1 = new Thread(m1);
+		//t1.start();
+		
+		
+		new Thread(new MultiThreadingInterface()).start();
 	}
 
-	@Override
-	public void addAddress() {
-		System.out.println("I am address inte");
-		
-	}
 }
